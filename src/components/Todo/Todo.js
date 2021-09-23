9f098160f94af4b9a2b85c9a2737d6452c07fecb
ellenkim <kim.ellen9@gmail.com>
2021-09-24 01:55:44 +0900

    completed practice session #4

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import './Todo.css';
+import "./Todo.css";
 
 const Todo = (props) => {
   return (
     <div className="Todo">
       <div
-        className={`text ${props.done && 'done'}`}
-        onClick={props.clicked}>
+        className={`text ${props.done && "done"}`}
+        onClick={props.clickDetail}
+      >
         {props.title}
       </div>
-      {props.done && <div className='done-mark'>&#x2713;</div>}
+      {props.done && <div className="done-mark">&#x2713;</div>}
+      <button onClick={props.clickDone}>
+        {props.done ? "Undone" : "Done"}
+      </button>
+      <button onClick={props.clickDelete}>Delete</button>{" "}
     </div>
   );
 };
---
+++ src/store/reducers/todo.js
@@ -0,0 +1,47 @@
+import * as actionTypes from "../action/actionType";
+
+const initialState = {
+  todos: [
+    { id: 1, title: "SWPP", content: "take swpp class", done: true },
+    { id: 2, title: "Movie", content: "watch movie", done: false },
+    { id: 3, title: "Dinner", content: "eat dinner", done: false },
+  ],
+  selectedTodo: null,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_TODO:
+      const newTodo = {
+        id: action.id,
+        title: action.title,
+        content: action.content,
+        done: action.done,
+      };
+      return { ...state, todos: [...state.todos, newTodo] };
+    case actionTypes.DELETE_TODO:
+      const deleted = state.todos.filter((todo) => {
+        return todo.id !== action.targetID;
+      });
+      return { ...state, todos: deleted };
+    case actionTypes.TOGGLE_DONE:
+      const modified = state.todos.map((todo) => {
+        if (todo.id === action.targetID) {
+          return { ...todo, done: !todo.done };
+        } else {
+          return { ...todo };
+        }
+      });
+      return { ...state, todos: modified };
+
+    case actionTypes.GET_TODO:
+      return { ...state, selectedTodo: action.target };
+
+    case actionTypes.GET_ALL:
+      return { ...state, todos: action.todos };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
