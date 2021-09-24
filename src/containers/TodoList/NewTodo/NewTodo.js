37554a5e1cd2a6b43b50b7fb813109591cbb7fd4
kyungwon-lee <kw375310@gmail.com>
2021-09-24 19:23:51 +0900

    Submit LAB4

--- src/containers/TodoList/NewTodo/NewTodo.js
+++ src/containers/TodoList/NewTodo/NewTodo.js
@@ -1,41 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { Redirect } from 'react-router-dom';
+import { Redirect } from "react-router-dom";
 
-import './NewTodo.css';
+import "./NewTodo.css";
+
+import { connect } from "react-redux";
+import * as actionCreators from "../../../store/action/index";
 
 class NewTodo extends Component {
   state = {
-    title: '',
-    content: '',
+    title: "",
+    content: "",
     submitted: false,
-  }
+  };
 
   postTodoHandler = () => {
-    const data = { title: this.state.title, content: this.state.content };
-    alert('Submitted\n' + data.title + '\n' + data.content);
+    this.props.onStoreTodo(this.state.title, this.state.content);
     this.setState({ submitted: true });
-  }
+  };
 
   render() {
     let redirect = null;
     if (this.state.submitted) {
-      redirect = <Redirect to='/todos' />
+      redirect = <Redirect to="/todos" />;
     }
     return (
       <div className="NewTodo">
         {redirect}
         <h1>Add a Todo</h1>
         <label>Title</label>
-        <input type="text" value={this.state.title}
-          onChange={(event) => this.setState({ title: event.target.value })} />
+        <input
+          type="text"
+          value={this.state.title}
+          onChange={(event) => this.setState({ title: event.target.value })}
+        />
         <label>Content</label>
-        <textarea rows="4" type="text" value={this.state.content}
-          onChange={(event) => this.setState({ content: event.target.value })} />
+        <textarea
+          rows="4"
+          type="text"
+          value={this.state.content}
+          onChange={(event) => this.setState({ content: event.target.value })}
+        />
         <button onClick={() => this.postTodoHandler()}>Submit</button>
       </div>
     );
   }
 }
 
-export default NewTodo;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onStoreTodo: (title, content) =>
+      dispatch(
+        actionCreators.postTodo({
+          title: title,
+          content: content,
+        })
+      ),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(NewTodo);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import todoReducer from "./store/reducers/todo";
+
+const reducer = (state = {}, action) => {
+  return state;
+};
+
+const rootReducer = combineReducers({
+  td: todoReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
