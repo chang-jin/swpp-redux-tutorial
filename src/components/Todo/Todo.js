b87bc4e488a19ed5ee3e72b99ab4d093f0530ad7
jungmin-cho-eng <jungmin97@snu.ac.kr>
2021-09-24 13:14:48 +0900

    Lab Session Followed

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -5,14 +5,33 @@ import './Todo.css';
 const Todo = (props) => {
   return (
     <div className="Todo">
-      <div
-        className={`text ${props.done && 'done'}`}
-        onClick={props.clicked}>
+      <div className={`text ${props.done && 'done'}`} onClick={props.clickDetail}>
         {props.title}
       </div>
-      {props.done && <div className='done-mark'>&#x2713;</div>}
+      {props.done && (<div className="done-mark">&#x2713;</div>)}
+      <button onClick={props.clickDone}>{(props.done) ? 'Undone' : 'Done'}</button>
+      <button onClick={props.clickDelete}>Delete</button>
     </div>
   );
-};
+}
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.types) {
+//     case actionTypes.DELETE_TODO:
+//       const deleted = state.todos.filter((todo) => {
+//         return todo.id !== action.targetID;
+//       });
+//       return { ...state, todos: deleted };
+//     case actionTypes.TOGGLE_DONE:
+//       const modified = state.todos.map((todo) => {
+//         if (todo.id === action.targetID) {
+//           return { ...todo, done: !todo.done };
+//         } else {
+//           return { ...todo };
+//         }
+//       });
+//       return { ...state, todos: modified };
+//   }
+// }
 
 export default Todo;
